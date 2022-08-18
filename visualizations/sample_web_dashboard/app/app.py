b19d9b43fa0970a85e8baa7855b8bc2ba7b57c02
Sibtain <muhamayu@microsoft.com>
2022-08-18 21:52:01 +0400

    end of file fixed

--- visualizations/sample_web_dashboard/app/app.py
+++ visualizations/sample_web_dashboard/app/app.py
@@ -61,5 +61,4 @@ def get_insights():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 8080), debug=True)
-    
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 8080), debug=True)    
